140327fe1155b6281ec2ead96b3b015f9f4e2287
Arber Muca <if19b164@technikum-wien.at>
2020-12-26 20:03:40 +0100

    new Classes for the http server taken from rest http

---
+++ src/main/java/ResponseStatusCode.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ResponseStatusCode {
+    OK(200,"OK"),
+    CREATED(201,"Created"),
+    ACCEPTED(202,"Accepted"),
+    NO_CONTENT(204,"No Content"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    NOT_MODIFIED(304,"Not Modified"),
+    BAD_REQUEST(400,"Bad Request"),
+    UNAUTHORISED(401,"Unauthorized"),
+    FORBIDDEN(403,"Forbidden"),
+    NOT_FOUND(404,"Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500,"Internal Server Error"),
+    NOT_IMPLEMENTED(501,"Not Implemented"),
+    BAD_GATEWAY(502,"Bad Gateway");
+
+
+    private int num;
+    private String desc;
+    private static Map<Integer,String> val;
+
+    ResponseStatusCode(int num, String desc){
+        this.num = num;
+        this.desc = desc;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getVal() {
+        return this.num + " " +this.desc;
+    }
+
+    private static void initMap(){
+        val = new HashMap<>();
+        for(ResponseStatusCode code: ResponseStatusCode.values()){
+            val.put(code.num, code.desc);
+        }
+    }
+    public static String getDesc(int number){
+        if(val == null){
+            initMap();
+        }
+        if(val.containsKey(number)){
+            return number + " " + val.get(number);
+        }
+        return "";
+    }
+}
