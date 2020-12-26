689884e54c0397c1a2065b5d9915845fb9ca6d7f
Arber Muca <if19b164@technikum-wien.at>
2020-12-26 20:05:40 +0100

    small changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         User usr = new User("Arber Muca");
         User opponent = new User("Norbert Niemand");
         Battlefield battle = new Battlefield();
         battle.startGame(usr,opponent);
-    }
+    }*/
 }
