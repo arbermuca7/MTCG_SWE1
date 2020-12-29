668113fae6990661cc6f155c5b7ea42af81b9914
Arber Muca <if19b164@technikum-wien.at>
2020-12-29 20:33:34 +0100

    small changes (bio and Image variable added)

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,10 +2,13 @@
 public class User{
     private String username;
     private String password;
+    private String bio;
+    private String image;
     private int coins = 20;
     private int user_status = 100;
     private Deck deck;
     private Stack stack;
+
 
     User(){
         username = "anonym";
@@ -26,6 +29,14 @@ public class User{
         deck = new Deck();
         stack = new Stack();
     }
+    User(String user, String pwd,String bio, String image){
+        username = user;
+        password = pwd;
+        this.bio = bio;
+        this.image = image;
+        deck = new Deck();
+        stack = new Stack();
+    }
 
     public String getUsername() {
         return username;
@@ -41,6 +52,22 @@ public class User{
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public int getCoins() {
